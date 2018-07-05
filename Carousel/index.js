966a404a60ec2a18f5855355416c2e78eee01c28
lisa <1432645541@QQ.com>
2018-07-05 08:45:18 +0800

    轮播图

---
+++ Carousel/index.js
@@ -0,0 +1,15 @@
+function handleCarousel(){
+	new Carousel({
+		id:'carousel',
+		aImg:[
+			'images/轮播1.jpg',
+			'images/轮播2.jpg',
+			'images/轮播3.jpg',
+			'images/轮播1.jpg',
+		],
+		width:728,
+		height:494,
+		playDuration:3000		
+	})
+}
+handleCarousel();
